748e3b2fd86bb5f184521053a67410ccbc77ff90
Prabhjot <rana.prabhjot@gmail.com>
2020-08-13 15:53:19 +1000

    season commit

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from "react";
+import "./SeasonDisplay.css";
 
-const SeasonDisplay=()=>{
-return <div>Season display</div>
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach",
+    iconName: "sun",
+    iconColor: "red",
+  },
+  winter: {
+    text: "Burp, its chilli",
+    iconName: "snowflake",
+    iconColor: "blue",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  // const text = season === "winter" ? "Burp, its chilli" : "Lets hit the beach";
+  // const icon=season === "winter" ? "snowflake" : "sun";
+  return (
+    <div className={`season-display ${season}`}>
+      <h1>
+        <i className={`icon-left huge ${iconName} icon`} />
+        {text}
+        <i className={`icon-right huge ${iconName} icon`} />
+      </h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,28 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDiplay from "./SeasonDisplay";
+import Loading from "./Loading";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMsg: null }; // only do direct assignment first time
+  state = { lat: null, errorMsg: null }; // only do direct assignment first time
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMsg: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMsg: err.message })
     );
   }
+
+  componentDidUpdate() {
+    console.log("Component updated");
+  }
+
   render() {
     if (this.state.lat && !this.state.errorMsg) {
-      return <div>Latitude:{this.state.lat}</div>;
+      return <SeasonDiplay lat={this.state.lat} />;
     }
     if (this.state.errorMsg && !this.state.lat) {
       return <div>Error:{this.state.errorMsg}</div>;
     }
-    return <div>Loading</div>;
+    return <Loading message="Please grant the location" />;
   }
 }
 
